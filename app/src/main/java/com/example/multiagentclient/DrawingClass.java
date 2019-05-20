725d0637e60993e189023f26233838485c32ac43
David Molinari <contact@davidmolinari.fr>
2019-05-20 13:49:33 +0200

    DrawincClass externe

---
+++ app/src/main/java/com/example/multiagentclient/DrawingClass.java
@@ -0,0 +1,28 @@
+package com.example.multiagentclient;
+
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+public class DrawingClass {
+
+    Path DrawingClassPath;
+    Paint DrawingClassPaint;
+
+    public Path getPath() {
+        return DrawingClassPath;
+    }
+
+    public void setPath(Path path) {
+        this.DrawingClassPath = path;
+    }
+
+
+    public Paint getPaint() {
+        return DrawingClassPaint;
+    }
+
+    public void setPaint(Paint paint) {
+        this.DrawingClassPaint = paint;
+    }
+}
