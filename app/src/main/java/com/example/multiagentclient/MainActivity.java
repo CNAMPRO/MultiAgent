7a862a98a519918597e6610e47c5d1838af66194
raimondylan <dylanraimon@gmail.com>
2019-05-20 10:35:06 +0200

    Ajout Canvas Paint

--- app/src/main/java/com/example/multiagentclient/MainActivity.java
+++ app/src/main/java/com/example/multiagentclient/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.multiagentclient;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +20,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    ConstraintLayout relativeLayout;
+    Paint paint;
+    View view;
+    Path path2;
+    Bitmap bitmap;
+    Canvas canvas;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +61,45 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        relativeLayout = (ConstraintLayout) findViewById(R.id.layout_main);
+
+        button = (Button)findViewById(R.id.button);
+
+        view = new SketchSheetView(MainActivity.this);
+
+        paint = new Paint();
+
+        path2 = new Path();
+
+        relativeLayout.addView(view, new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,                ConstraintLayout.LayoutParams.MATCH_PARENT));
+
+        paint.setDither(true);
+
+        paint.setColor(Color.parseColor("#000000"));
+
+        paint.setStyle(Paint.Style.STROKE);
+
+        paint.setStrokeJoin(Paint.Join.ROUND);
+
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        paint.setStrokeWidth(2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                path2.reset();
+
+            }
+        });
+
+
+
     }
 
     @Override
@@ -85,4 +145,85 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    class SketchSheetView extends View {
+
+        public SketchSheetView(Context context) {
+
+            super(context);
+
+            bitmap = Bitmap.createBitmap(820, 480, Bitmap.Config.ARGB_4444);
+
+            canvas = new Canvas(bitmap);
+
+            this.setBackgroundColor(Color.WHITE);
+        }
+
+        private ArrayList<DrawingClass> DrawingClassArrayList = new ArrayList<DrawingClass>();
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+
+            DrawingClass pathWithPaint = new DrawingClass();
+
+            canvas.drawPath(path2, paint);
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                path2.moveTo(event.getX(), event.getY());
+
+                path2.lineTo(event.getX(), event.getY());
+            }
+            else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+                path2.lineTo(event.getX(), event.getY());
+
+                pathWithPaint.setPath(path2);
+
+                pathWithPaint.setPaint(paint);
+
+                DrawingClassArrayList.add(pathWithPaint);
+            }
+
+            invalidate();
+            return true;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (DrawingClassArrayList.size() > 0) {
+
+                canvas.drawPath(
+                        DrawingClassArrayList.get(DrawingClassArrayList.size() - 1).getPath(),
+
+                        DrawingClassArrayList.get(DrawingClassArrayList.size() - 1).getPaint());
+            }
+        }
+    }
+
+    public class DrawingClass {
+
+        Path DrawingClassPath;
+        Paint DrawingClassPaint;
+
+        public Path getPath() {
+            return DrawingClassPath;
+        }
+
+        public void setPath(Path path) {
+            this.DrawingClassPath = path;
+        }
+
+
+        public Paint getPaint() {
+            return DrawingClassPaint;
+        }
+
+        public void setPaint(Paint paint) {
+            this.DrawingClassPaint = paint;
+        }
+    }
 }
+
+
